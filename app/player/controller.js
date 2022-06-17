38223a5130a0577b44ba9935696fc8b519ce8581
AzraYazid <azrayazidalkautsar@gmail.com>
2022-06-17 18:57:11 +0700

    update API

--- app/player/controller.js
+++ app/player/controller.js
@@ -29,7 +29,7 @@ module.exports = {
         return res.status(404).json({ message: 'voucher game tidak ditemukan.!' });
       }
 
-      res.status(200).json({ details: voucher });
+      res.status(200).json({ data: voucher });
     } catch (err) {
       res.status(500).json({ message: err.message || `Internal server error` });
     }
