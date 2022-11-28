0a08806bc28c9228db0a31f5728146096a5bed56
AzraYazid <azrayazidalkautsar@gmail.com>
2022-11-28 09:42:06 +0700

    update API

--- app/player/controller.js
+++ app/player/controller.js
@@ -12,7 +12,7 @@ const config = require('../../config');
 module.exports = {
   landingPage: async (req, res) => {
     try {
-      const voucher = await Voucher.find().select('_id name status category thumbnail').populate('category');
+      const voucher = await Voucher.find({ status: 'Y' }).select('_id name status category thumbnail').populate('category');
 
       res.status(200).json({ data: voucher });
     } catch (err) {
